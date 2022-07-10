cd22c3237fd688bee0db6f48b9bc493ca903e61a
coolraku <107172092+coolraku@users.noreply.github.com>
2022-07-10 23:09:25 +0530

    Delete vnetmodule directory

--- vnetmodule/locals.tf
+++
@@ -1,8 +0,0 @@
-# Define Local Values in Terraform
-locals {
-  environment = var.environment
-  resource_name_prefix = var.prefix
-  resource_type = var.resource_type
-  app_name = var.appname
-  azure_region = var.resource_group_location
-} 
